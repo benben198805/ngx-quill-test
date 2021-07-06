5e05a026a7b2d60155b6e2b8a097d827da7d47cb
qlyan <qlyan@thoughtworks.com>
2021-07-06 17:35:31 +0800

    add test component

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -7,58 +7,67 @@ import { ChildModule } from './child-module/child-module'
 
 import { AppComponent } from './app.component'
 import Counter from './counter'
+import Smaller from './smaller'
+import Bigger from './bigger'
+import ImageModal from './image-modal'
 
 import { MatFormFieldModule } from '@angular/material/form-field'
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations'
 import { MatQuillModule } from './mat-quill/mat-quill-module'
 
 import { ActivateRtlComponent } from './activate-rtl/activate-rtl.component'
-import { BubbleEditorComponent } from './bubble-editor/bubble-editor.component'
-import { CustomToolbarComponent } from './custom-toolbar/custom-toolbar.component'
+// import { BubbleEditorComponent } from './bubble-editor/bubble-editor.component'
+// import { CustomToolbarComponent } from './custom-toolbar/custom-toolbar.component'
 import { DefaultComponent } from './default/default.component'
-import { EmojiComponent } from './emoji/emoji.component'
-import { FormatHtmlComponent } from './format-html/format-html.component'
-import { FormatJsonComponent } from './format-json/format-json.component'
-import { FormatObjectComponent } from './format-object/format-object.component'
-import { FormatTextComponent } from './format-text/format-text.component'
-import { FormulaComponent } from './formula/formula.component'
-import { MentionComponent } from './mention/mention.component'
-import { ModelValidation } from './model-validation/model-validation.component'
-import { NoSanitizeHtmlComponent } from './no-sanitize-html/no-sanitize-html.component'
-import { NoToolbarComponent } from './no-toolbar/no-toolbar.component'
-import { PreserveWhitespaceComponent } from './preserve-whitespace/preserve-whitespace.component'
-import { QuillApiEmbedComponent } from './quill-api-embed/quill-api-embed.component'
-import { ReactiveFormsComponent } from './reactive-forms/reactive-forms.component'
-import { SanitizeHtmlComponent } from './sanitize-html/sanitize-html.component'
-import { ViewHTMLComponent } from './view-html/view-html.component'
-import { ViewComponent } from './view/view.component'
+import { TestComponent } from './test/test.component'
+import { TestModalComponent } from './test-modal/test-modal.component'
+import { MatDialogModule } from '@angular/material/dialog'
+// import { EmojiComponent } from './emoji/emoji.component'
+// import { FormatHtmlComponent } from './format-html/format-html.component'
+// import { FormatJsonComponent } from './format-json/format-json.component'
+// import { FormatObjectComponent } from './format-object/format-object.component'
+// import { FormatTextComponent } from './format-text/format-text.component'
+// import { FormulaComponent } from './formula/formula.component'
+// import { MentionComponent } from './mention/mention.component'
+// import { ModelValidation } from './model-validation/model-validation.component'
+// import { NoSanitizeHtmlComponent } from './no-sanitize-html/no-sanitize-html.component'
+// import { NoToolbarComponent } from './no-toolbar/no-toolbar.component'
+// import { PreserveWhitespaceComponent } from './preserve-whitespace/preserve-whitespace.component'
+// import { QuillApiEmbedComponent } from './quill-api-embed/quill-api-embed.component'
+// import { ReactiveFormsComponent } from './reactive-forms/reactive-forms.component'
+// import { SanitizeHtmlComponent } from './sanitize-html/sanitize-html.component'
+// import { ViewHTMLComponent } from './view-html/view-html.component'
+// import { ViewComponent } from './view/view.component'
 
 @NgModule({
   bootstrap: [AppComponent],
   declarations: [
     AppComponent,
+    TestComponent,
+    TestModalComponent,
     DefaultComponent,
     ActivateRtlComponent,
-    SanitizeHtmlComponent,
-    FormulaComponent,
-    ReactiveFormsComponent,
-    EmojiComponent,
-    BubbleEditorComponent,
-    MentionComponent,
-    NoToolbarComponent,
-    CustomToolbarComponent,
-    FormatObjectComponent,
-    FormatTextComponent,
-    NoSanitizeHtmlComponent,
-    FormatJsonComponent,
-    FormatHtmlComponent,
-    PreserveWhitespaceComponent,
-    QuillApiEmbedComponent,
-    ViewComponent,
-    ViewHTMLComponent,
-    ModelValidation
+    // SanitizeHtmlComponent,
+    // FormulaComponent,
+    // ReactiveFormsComponent,
+    // EmojiComponent,
+    // BubbleEditorComponent,
+    // MentionComponent,
+    // NoToolbarComponent,
+    // CustomToolbarComponent,
+    // FormatObjectComponent,
+    // FormatTextComponent,
+    // NoSanitizeHtmlComponent,
+    // FormatJsonComponent,
+    // FormatHtmlComponent,
+    // PreserveWhitespaceComponent,
+    // QuillApiEmbedComponent,
+    // ViewComponent,
+    // ViewHTMLComponent,
+    // ModelValidation
   ],
   imports: [
+    MatDialogModule,
     BrowserModule,
     BrowserAnimationsModule,
     FormsModule,
@@ -68,6 +77,15 @@ import { ViewComponent } from './view/view.component'
       customModules: [{
         implementation: Counter,
         path: 'modules/counter'
+      },{
+        implementation: Smaller,
+        path: 'modules/smaller'
+      },{
+        implementation: Bigger,
+        path: 'modules/bigger'
+      },{
+        implementation: ImageModal,
+        path: 'modules/imageModal'
       }],
       customOptions: [{
         import: 'formats/font',
---
+++ src/app/test/test.component.ts
@@ -0,0 +1,70 @@
+import { Component, NgZone } from '@angular/core'
+import { MatDialog } from '@angular/material/dialog';
+import { TestModalComponent } from '../test-modal/test-modal.component'
+
+export interface DialogData {
+  animal: string;
+  name: string;
+}
+
+@Component({
+  selector: 'app-test',
+  templateUrl: './test.component.html',
+  styleUrls: ['./test.component.css']
+})
+export class TestComponent {
+
+  editor;
+  isReadOnly = false;
+  constructor(public dialog: MatDialog, private ngZone: NgZone) {
+  }
+
+  createdQuill(editor) {
+    this.editor = editor;
+  }
+
+  disable() {
+    this.editor.disable();
+  }
+
+  enable() {
+    this.editor.enable();
+  }
+
+  openDialog = () => {
+    this.ngZone.run(() => {
+      this.dialog.open(TestModalComponent, {
+        width: '600px', height: '600px'
+      }).afterClosed().subscribe(res => {
+        const range = this.editor.getSelection(true)
+        this.editor.insertEmbed(range.index, 'image', 'assets/test.svg', 'user')
+        var container = document.getElementById("tempSvg");
+        container.innerHTML = "<svg>" + res.data + "</svg>";
+        console.log(1)
+        this.editor.insertEmbed(range.index, 'text', res.data, 'user');
+        console.log(2)
+      }, error => console.log(error));
+    });
+  }
+
+  toggleReadOnly() {
+    this.isReadOnly = !this.isReadOnly;
+  }
+
+  modules = {
+    counter: { container: '#counter', unit: 'word' },
+    smaller: { container: '#smaller' },
+    bigger: { container: '#bigger' },
+    imageModal: {
+      container: '#image-modal',
+      click: this.openDialog
+    }
+  };
+
+  editorModel = [{
+    attributes: {
+      font: 'roboto'
+    },
+    insert: 'test'
+  }]
+}
